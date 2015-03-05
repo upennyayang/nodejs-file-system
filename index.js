84e6926cff53686c6910246ce10ff3c6ce2b2ad0
Yayang Tian <ytian@walmartlabs.com>
2015-03-04 20:33:55 -0800

    Added URL parsing support.

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
 let http = require('http')
 let fs = require('fs')
+let url = require('url')
+var querystring = require('querystring')
+let path = require('path')
+let _ = require('lodash')
+
 
 require('songbird')
 async function ls(dirPath) {
-    let files = await fs.promise.readdir(dirPath)
+    let stat = await fs.promise.stat(dirPath)
+    if(!stat.isDirectory()) return [dirPath]
 
-    let directFiles = []
-    for(let name of files) {
-        let stat = await fs.promise.stat(name)
-        if (!stat.isDirectory(name)) {
-            directFiles.push(name)
-        }
+    let promises = []
+    for(let name of await fs.promise.readdir(dirPath)) {
+        // console.log(name, ":", path.join(dirPath, name))
+        let promise = ls(path.join(dirPath, name))
+        promises.push(promise)
     }
-    return JSON.stringify(directFiles)
+    return JSON.stringify(_.flatten(await promises))
 }
 
 http.createServer((req, res) => {
 
     async() => {
       res.setHeader('Content-Type', 'application/json')
-      let files = await ls(__dirname)
+      let urlobject = url.parse(req.url)
+      let pathname = urlobject.pathname
+      let query = urlobject.query
 
-      res.end(files)
+      // parse querystring
+      let store = querystring.parse(query).store
+
+      if(store === 'local') {
+        let files = await ls(pathname)
+        res.end(files)
+
+      } else if(store === 'dropbox') {
+        res.end('dropbox')
+      }
+
     }().catch(e => console.log(e.stack))
 
 }).listen(8000, '127.0.0.1')
