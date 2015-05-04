b8de433d2e8482fe18c583b2cafe7e43f28b98c3
Yayang Tian <ytian@walmartlabs.com>
2015-05-03 22:05:31 -0700

    Implemented async await readdir.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 let http = require('http')
+let fs = require('fs')
 
 http.createServer((req, res) => {
-    res.writeHeader(200, {'Content-Type': 'text/plain'})
-    res.end('Hello World\n')
+
+    async() => {
+      res.setHeader('Content-Type', 'application/json')
+      let files = await ls(__dirname)
+
+      res.end(files)
+    }().catch(e => console.log(e.stack))
+
 }).listen(8000, '127.0.0.1')
 
-console.log('Server running at http://127.0.0.1:8000')
+console.log('Server running at http://127.0.0.1:8000/')
 
+require('songbird')
+async function ls(dirPath) {
+    let files = await fs.promise.readdir(dirPath)
 
+    return JSON.stringify(files)
+}
